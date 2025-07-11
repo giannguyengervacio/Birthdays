7c2143dad7a9b4e370bb5119dd51353cf6a3777d
gianne <giannguyengervacio@gmail.com>
2025-07-11 16:10:39 +0200

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Orvs on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
